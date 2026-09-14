b84dc098f4d83e2df57c6dc9e7b33b2f9ee2d43f
Kelben-ui <njock.awoh@gmail.com>
2026-09-15 00:16:54 +0100

    Complete Day 2 load balancing and TLS setup

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import time
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -8,14 +10,26 @@ def home():
     host = request.headers.get("Host")
     real_ip = request.headers.get("X-Real-IP")
     forwarded = request.headers.get("X-Forwarded-For")
+    port = request.environ.get("SERVER_PORT")
 
     print(f"BACKEND LOG: Client IP={real_ip}, Host={host}, Forwarded For={forwarded}")
 
     return f"""
+Backend Port = {port}<br>
 Host = {host}<br>
 Real IP = {real_ip}<br>
 Forwarded For = {forwarded}
 """
 
+@app.route("/slow")
+def slow():
+
+    time.sleep(10)
+
+    port = request.environ.get("SERVER_PORT")
+
+    return f"Backend Port = {port}<br>Slow request completed"
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="127.0.0.1", port=port)
